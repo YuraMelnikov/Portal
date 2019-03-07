a2566e85a028758ebc37eddd1eb40eed593149c1
Юрий Мельников <yury.melnikau@gmail.com>
2019-03-07 11:31:57 +0300

    -

---
+++ Wiki/Areas/PZ/Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Linq.Dynamic;
+using System.Web.Mvc;
+using DataTables;
+
+namespace Wiki.Areas.PZ.Controllers
+{
+    public class OrderController : Controller
+    {
+        PortalKATEKEntities db = new PortalKATEKEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Table()
+        {
+            var formData = HttpContext.Request.Form;
+            var response = db.PZ_PlanZakaz.ToList();
+
+            return Json(response, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
