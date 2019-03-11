1f0faa7515cab2052d770aae99412693f1281ed9
Юрий Мельников <yury.melnikau@gmail.com>
2019-03-11 09:23:32 +0300

    PZ - Buttons planZakaz lists

--- Wiki/Areas/PZ/Controllers/OrderController.cs
+++ Wiki/Areas/PZ/Controllers/OrderController.cs
@@ -17,8 +17,95 @@ namespace Wiki.Areas.PZ.Controllers
         [HttpPost]
         public JsonResult OrdersList()
         {
-            var query = db.PZ_PlanZakaz.ToList();
+            int countLastOrdersView = 200;
+            var query = db.PZ_PlanZakaz.OrderByDescending(d => d.DateCreate).Take(countLastOrdersView).ToList();
 
+            var data = query.Select(dataList => new
+            {
+                dataList.PlanZakaz,
+                dataList.PZ_ProductType.ProductType,
+                DateCreate = JsonConvert.SerializeObject(dataList.DateCreate, settings).Replace(@"""", ""),
+                StatusOrder = "",
+                Manager = dataList.AspNetUsers.CiliricalName,
+                dataList.Description,
+                dataList.MTR,
+                dataList.nomenklaturNumber,
+                dataList.Name,
+                dataList.nameTU,
+                dataList.OL,
+                dataList.PZ_Client.NameSort,
+                dataList.Zapros,
+                dataList.numZakupki,
+                dataList.numLota,
+                dataList.timeContract,
+                timeContractDate = JsonConvert.SerializeObject(dataList.timeContractDate, settings).Replace(@"""", ""),
+                dataList.timeArr,
+                timeArrDate = JsonConvert.SerializeObject(dataList.timeArrDate, settings).Replace(@"""", ""),
+                DateShipping = JsonConvert.SerializeObject(dataList.DateShipping, settings).Replace(@"""", ""),
+                DateSupply = JsonConvert.SerializeObject(dataList.DateSupply, settings).Replace(@"""", ""),
+                OperatorDogovora = dataList.PZ_OperatorDogovora.name,
+                KuratorDogovora = dataList.PZ_FIO.fio,
+                typeDostavka = dataList.PZ_Dostavka.Name,
+                dataList.Cost,
+                dataList.costSMR,
+                dataList.costPNR,
+                SF = "",
+                dataOtgruzkiBP = JsonConvert.SerializeObject(dataList.dataOtgruzkiBP, settings).Replace(@"""", ""),
+                dateDostavki = "",
+                datePriemki = "",
+                dateOplat = ""
+            });
+
+            return Json(new { data });
+        }
+
+        [HttpPost]
+        public JsonResult OrdersListLY(int yearCreateOrder)
+        {
+            var query = db.PZ_PlanZakaz.Where(d => d.DateCreate.Year == yearCreateOrder).ToList();
+            var data = query.Select(dataList => new
+            {
+                dataList.PlanZakaz,
+                dataList.PZ_ProductType.ProductType,
+                DateCreate = JsonConvert.SerializeObject(dataList.DateCreate, settings).Replace(@"""", ""),
+                StatusOrder = "",
+                Manager = dataList.AspNetUsers.CiliricalName,
+                dataList.Description,
+                dataList.MTR,
+                dataList.nomenklaturNumber,
+                dataList.Name,
+                dataList.nameTU,
+                dataList.OL,
+                dataList.PZ_Client.NameSort,
+                dataList.Zapros,
+                dataList.numZakupki,
+                dataList.numLota,
+                dataList.timeContract,
+                timeContractDate = JsonConvert.SerializeObject(dataList.timeContractDate, settings).Replace(@"""", ""),
+                dataList.timeArr,
+                timeArrDate = JsonConvert.SerializeObject(dataList.timeArrDate, settings).Replace(@"""", ""),
+                DateShipping = JsonConvert.SerializeObject(dataList.DateShipping, settings).Replace(@"""", ""),
+                DateSupply = JsonConvert.SerializeObject(dataList.DateSupply, settings).Replace(@"""", ""),
+                OperatorDogovora = dataList.PZ_OperatorDogovora.name,
+                KuratorDogovora = dataList.PZ_FIO.fio,
+                typeDostavka = dataList.PZ_Dostavka.Name,
+                dataList.Cost,
+                dataList.costSMR,
+                dataList.costPNR,
+                SF = "",
+                dataOtgruzkiBP = JsonConvert.SerializeObject(dataList.dataOtgruzkiBP, settings).Replace(@"""", ""),
+                dateDostavki = "",
+                datePriemki = "",
+                dateOplat = ""
+            });
+
+            return Json(new { data });
+        }
+
+        [HttpPost]
+        public JsonResult OrdersListALL()
+        {
+            var query = db.PZ_PlanZakaz.ToList();
             var data = query.Select(dataList => new
             {
                 dataList.PlanZakaz,
