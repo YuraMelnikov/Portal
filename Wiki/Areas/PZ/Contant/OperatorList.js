97599505312dbee6b8aa4ad358949afbe74ce807
Юрий Мельников <yury.melnikau@gmail.com>
2019-03-14 13:57:08 +0300

    PZ_Operator: - CRUD & View complited

---
+++ Wiki/Areas/PZ/Contant/OperatorList.js
@@ -0,0 +1,135 @@
+﻿$(document).ready(function () {
+    loadData();
+});
+
+function loadData() {
+    $("#myTable").DataTable({
+        "ajax": {
+            "url": "/Operator/OperatorList",
+            "type": "POST",
+            "datatype": "json"
+        },
+        "bDestroy": true,
+        "bAutoWidth": false,
+        "order": [1],
+        "columns": [
+            { "title": "Ред", "data": "Id", "autowidth": true, "bSortable": false },
+            { "title": "Наименование", "data": "name", "autowidth": true }
+        ],
+        "scrollY": '75vh',
+        "scrollX": true,
+        "paging": false,
+        "info": false,
+        "language": {
+            "zeroRecords": "Отсутствуют записи",
+            "infoEmpty": "Отсутствуют записи",
+            "search": "Поиск"
+        }
+    });
+}
+
+function Add() {
+    var res = validate();
+    if (res === false) {
+        return false;
+    }
+    $("#btnAdd").attr('disabled', true);
+    var typeObj = {
+        id: $('#id').val(),
+        name: $('#name').val()
+    };
+    $.ajax({
+        url: "/Operator/Add",
+        data: JSON.stringify(typeObj),
+        type: "POST",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            loadData();
+            $('#orderModal').modal('hide');
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+}
+
+function validate() {
+    var isValid = true;
+    if ($('#name').val() === null) {
+        $('#name').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#name').css('border-color', 'lightgrey');
+    }
+
+    if ($('#id').val() === null) {
+        $('#id').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#id').css('border-color', 'lightgrey');
+    }
+
+    return isValid;
+}
+
+function clearTextBox() {
+    $("#btnAdd").attr('disabled', false);
+    $('#id').val("");
+    $('#name').val("");
+    $('#btnUpdate').hide();
+    $('#btnAdd').show();
+    $('#name').css('border-color', 'lightgrey');
+    $('#active').css('border-color', 'lightgrey');
+}
+
+function getbyID(Id) {
+    $('#name').css('border-color', 'lightgrey');
+    $('#active').css('border-color', 'lightgrey');
+    $.ajax({
+        url: "/Operator/GetOperator/" + Id,
+        typr: "GET",
+        contentType: "application/json;charset=UTF-8",
+        dataType: "json",
+        success: function (result) {
+            $('#id').val(result.id);
+            $('#name').val(result.name);
+            $('#orderModal').modal('show');
+            $('#btnUpdate').show();
+            $('#btnAdd').hide();
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+    return false;
+}
+
+function Update() {
+    var res = validate();
+    if (res === false) {
+        return false;
+    }
+    var typeObj = {
+        id: $('#id').val(),
+        name: $('#name').val()
+    };
+    $.ajax({
+        url: "/Operator/Update",
+        data: JSON.stringify(typeObj),
+        type: "POST",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            loadData();
+            $('#orderModal').modal('hide');
+            $('#id').val("");
+            $('#name').val("");
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+}
---
+++ Wiki/Areas/PZ/Controllers/OperatorController.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Wiki.Areas.PZ.Controllers
+{
+    public class OperatorController : Controller
+    {
+        PortalKATEKEntities db = new PortalKATEKEntities();
+        readonly JsonSerializerSettings settings = new JsonSerializerSettings { DateFormatString = "dd.MM.yyyy" };
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult OperatorList()
+        {
+            var query = db.PZ_OperatorDogovora.OrderBy(d => d.name).ToList();
+            var data = query.Select(dataList => new
+            {
+                Id = "<td><a href=" + '\u0022' + "#" + '\u0022' + " onclick=" + '\u0022' + "return getbyID('" + dataList.id + "')" + '\u0022' + "><span class=" + '\u0022' + "glyphicon glyphicon-pencil" + '\u0022' + "></span></a></td>",
+                dataList.name
+            });
+
+            return Json(new { data });
+        }
+
+        public JsonResult Add(PZ_OperatorDogovora operatorDogovora)
+        {
+            db.PZ_OperatorDogovora.Add(operatorDogovora);
+            db.SaveChanges();
+
+            return Json(1, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetOperator(int Id)
+        {
+            var query = db.PZ_OperatorDogovora.Where(d => d.id == Id).ToList();
+            var data = query.Select(dataList => new
+            {
+                dataList.id,
+                dataList.name
+            });
+
+            return Json(data.First(), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Update(PZ_OperatorDogovora operatorDogovora)
+        {
+            db.Entry(operatorDogovora).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+
+            return Json(1, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
