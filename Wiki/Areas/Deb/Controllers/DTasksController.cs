433544c53fc9ad671a1deb25c526696a1c7a15ea
Юрий Мельников <yury.melnikau@gmail.com>
2019-03-15 12:46:34 +0300

    Debit: - add area

---
+++ Wiki/Areas/Deb/Controllers/DTasksController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Wiki.Areas.Deb.Controllers
+{
+    public class DTasksController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Wiki/Areas/Deb/DebAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Wiki.Areas.Deb
+{
+    public class DebAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Deb";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Deb_default",
+                "Deb/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
