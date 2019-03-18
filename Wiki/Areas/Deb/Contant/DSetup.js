645d33babd000c6218ccd021a8f93fbc153f3695
Юрий Мельников <yury.melnikau@gmail.com>
2019-03-18 14:10:56 +0300

    Debit: - contract setup list - complited

---
+++ Wiki/Areas/Deb/Contant/DSetup.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+    loadData();
+});
+
+function loadData() {
+    $("#myTable").DataTable({
+        "ajax": {
+            "url": "/DSetup/List",
+            "type": "POST",
+            "datatype": "json"
+        },
+        "order": [[0, "desc"]],
+        "bDestroy": true,
+        "bAutoWidth": false,
+        "columns": [
+            { "title": "Номер", "data": "PlanZakaz", "autowidth": true }
+            , { "title": "Менеджер", "data": "Manager", "autowidth": true }
+            , { "title": "Заказчик", "data": "Client", "autowidth": true }
+
+            , { "title": "Кол-во дней на приемку", "data": "KolVoDneyNaPrijemku", "autowidth": true }
+            , { "title": "Условия приемки изделия", "data": "PunktDogovoraOSrokahPriemki", "autowidth": true }
+            , { "title": "Условия оплаты", "data": "UslovieOplatyText", "autowidth": true }
+             ],
+        "scrollY": '75vh',
+        "scrollX": true,
+        "paging": false,
+        "info": false,
+        "language": {
+            "zeroRecords": "Отсутствуют записи",
+            "infoEmpty": "Отсутствуют записи",
+            "search": "Поиск"
+        }
+    });
+}
---
+++ Wiki/Areas/Deb/Controllers/DSetupController.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Wiki.Areas.Deb.Controllers
+{
+    public class DSetupController : Controller
+    {
+        PortalKATEKEntities db = new PortalKATEKEntities();
+        readonly JsonSerializerSettings settings = new JsonSerializerSettings { DateFormatString = "dd.MM.yyyy" };
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult List()
+        {
+            var query = db.PZ_Setup.Where(d => d.PZ_PlanZakaz.DateCreate.Year > 2017).Where(d => d.PZ_PlanZakaz.Client != 39).ToList();
+            var data = query.Select(dataList => new
+            {
+                dataList.PZ_PlanZakaz.PlanZakaz,
+                Manager = dataList.PZ_PlanZakaz.AspNetUsers.CiliricalName,
+                Client = dataList.PZ_PlanZakaz.PZ_Client.NameSort,
+                dataList.KolVoDneyNaPrijemku,
+                dataList.PunktDogovoraOSrokahPriemki,
+                dataList.UslovieOplatyText
+            });
+
+            return Json(new { data });
+        }
+    }
+}
