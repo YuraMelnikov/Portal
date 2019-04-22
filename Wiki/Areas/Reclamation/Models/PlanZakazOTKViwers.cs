7b06ea73fb6ab434ebba4a412d11c30d6cc77832
Юрий Мельников <yury.melnikau@gmail.com>
2019-04-22 09:24:08 +0300

    Reclamation: - add class OTK Viwers

---
+++ Wiki/Areas/Reclamation/Models/PlanZakazOTKViwers.cs
@@ -0,0 +1,17 @@
+﻿namespace Wiki.Areas.Reclamation.Models
+{
+    public struct PlanZakazOTKViwers
+    {
+        int id_PZ_PlanZakaz;
+        int planZakaz;
+        string contractName;
+        string tuName;
+        string client;
+        string mtr;
+        string ol;
+        int reclamationCount;
+        int reclamationActive;
+        int reclamationNotAnswer;
+        int reclamationClose;
+    }
+}
---
+++ Wiki/Areas/Reclamation/Models/ReclamationOTKViwers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Wiki.Areas.Reclamation.Models
+{
+    public struct ReclamationOTKViwers
+    {
+        int id_Reclamation;
+        int planZakaz;
+        string type;
+        string close;
+        string text;
+        string description;
+        float timeToSearch;
+        float timeToEliminate;
+        string answers;
+        string devision;
+        DateTime dateCreate;
+        DateTime dateLastAnswer;
+    }
+}
