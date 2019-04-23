7e9eb8435a3eac5cb1010acf7648c8ed97bd1f65
Юрий Мельников <yury.melnikau@gmail.com>
2019-04-23 15:15:10 +0300

    Reclamation: - pz lists

---
+++ Wiki/Areas/Reclamation/Models/PlanZakazListViewers.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Wiki.Areas.Reclamation.Models
+{
+    public class PlanZakazListViewers
+    {
+        PortalKATEKEntities db = new PortalKATEKEntities();
+        List<PlanZakazViwers> planZakazViwers;
+
+        public List<PlanZakazViwers> PlanZakazViwers { get => planZakazViwers; set => planZakazViwers = value; }
+
+        void InitializationList()
+        {
+            planZakazViwers = new List<PlanZakazViwers>();
+        }
+
+        public void GetPlanZakazs()
+        {
+            InitializationList();
+            foreach (var data in db.PZ_PlanZakaz.ToList())
+            {
+                PlanZakazViwers planZakazViwers = new PlanZakazViwers(data);
+                PlanZakazViwers.Add(planZakazViwers);
+            }
+        }
+
+        public void GetPlanZakazs(int id_Devision)
+        {
+            InitializationList();
+            foreach (var data in db.PZ_PlanZakaz.ToList())
+            {
+                PlanZakazViwers planZakazViwers = new PlanZakazViwers(data, id_Devision);
+                PlanZakazViwers.Add(planZakazViwers);
+            }
+        }
+
+        public void GetPlanZakazs(int id_Devision, bool sh)
+        {
+            InitializationList();
+            List<PZ_PlanZakaz> list = new List<PZ_PlanZakaz>();
+            if (sh == true)
+                list = db.PZ_PlanZakaz.Where(d => d.dataOtgruzkiBP < DateTime.Now).ToList();
+            else
+                list = db.PZ_PlanZakaz.Where(d => d.dataOtgruzkiBP >= DateTime.Now).ToList();
+            foreach (var data in db.PZ_PlanZakaz.ToList())
+            {
+                PlanZakazViwers planZakazViwers = new PlanZakazViwers(data, id_Devision);
+                PlanZakazViwers.Add(planZakazViwers);
+            }
+        }
+    }
+}
--- Wiki/Areas/Reclamation/Models/PlanZakazViwers.cs
+++ Wiki/Areas/Reclamation/Models/PlanZakazViwers.cs
@@ -1,6 +1,8 @@
-﻿namespace Wiki.Areas.Reclamation.Models
+﻿using System.Linq;
+
+namespace Wiki.Areas.Reclamation.Models
 {
-    public struct PlanZakazViwers
+    public class PlanZakazViwers
     {
         int id_PZ_PlanZakaz;
         int planZakaz;
@@ -11,7 +13,6 @@
         string ol;
         int reclamationCount;
         int reclamationActive;
-        int reclamationNotAnswer;
         int reclamationClose;
 
         public int Id_PZ_PlanZakaz { get => id_PZ_PlanZakaz; set => id_PZ_PlanZakaz = value; }
@@ -23,7 +24,73 @@
         public string Ol { get => ol; set => ol = value; }
         public int ReclamationCount { get => reclamationCount; set => reclamationCount = value; }
         public int ReclamationActive { get => reclamationActive; set => reclamationActive = value; }
-        public int ReclamationNotAnswer { get => reclamationNotAnswer; set => reclamationNotAnswer = value; }
         public int ReclamationClose { get => reclamationClose; set => reclamationClose = value; }
+
+        public PlanZakazViwers(PZ_PlanZakaz pZ_PlanZakaz)
+        {
+            Id_PZ_PlanZakaz = pZ_PlanZakaz.Id;
+            PlanZakaz = pZ_PlanZakaz.PlanZakaz;
+            ContractName = pZ_PlanZakaz.Name;
+            TuName = pZ_PlanZakaz.nameTU;
+            Client = pZ_PlanZakaz.PZ_Client.NameSort;
+            Mtr = pZ_PlanZakaz.MTR;
+            Ol = pZ_PlanZakaz.OL;
+            ReclamationCount = pZ_PlanZakaz.Reclamation_PZ.Count;
+            ReclamationActive = pZ_PlanZakaz.Reclamation_PZ.Where(d => d.Reclamation.close == false).Count();
+            ReclamationClose = pZ_PlanZakaz.Reclamation_PZ.Where(d => d.Reclamation.close == true).Count();
+        }
+
+        public PlanZakazViwers(PZ_PlanZakaz pZ_PlanZakaz, int id_Devision)
+        {
+            Id_PZ_PlanZakaz = pZ_PlanZakaz.Id;
+            PlanZakaz = pZ_PlanZakaz.PlanZakaz;
+            ContractName = pZ_PlanZakaz.Name;
+            TuName = pZ_PlanZakaz.nameTU;
+            Client = pZ_PlanZakaz.PZ_Client.NameSort;
+            Mtr = pZ_PlanZakaz.MTR;
+            Ol = pZ_PlanZakaz.OL;
+            ReclamationCount = GetReclamationCount(pZ_PlanZakaz, id_Devision);
+            ReclamationActive = GetReclamationActive(pZ_PlanZakaz, id_Devision, false);
+            ReclamationClose = GetReclamationActive(pZ_PlanZakaz, id_Devision, true);
+        }
+
+        int GetReclamationCount(PZ_PlanZakaz pZ_PlanZakaz, int id_Devision)
+        {
+            int count = 0;
+            if (id_Devision == 6)
+                count = pZ_PlanZakaz.Reclamation_PZ.Where(d => d.Reclamation.id_DevisionCreate == 6).Count();
+            else if (id_Devision == 3 || id_Devision == 16)
+                count = pZ_PlanZakaz.Reclamation_PZ.Where(d => d.Reclamation.id_DevisionReclamation == 3 || d.Reclamation.id_DevisionReclamation == 16).Count();
+            else if (id_Devision == 0)
+                count = pZ_PlanZakaz.Reclamation_PZ.Count();
+            else
+                count = pZ_PlanZakaz.Reclamation_PZ.Where(d => d.Reclamation.id_DevisionReclamation == id_Devision).Count();
+            return count;
+        }
+
+        int GetReclamationActive(PZ_PlanZakaz pZ_PlanZakaz, int id_Devision, bool close)
+        {
+            int count = 0;
+            if (id_Devision == 6)
+                count = pZ_PlanZakaz.Reclamation_PZ
+                    .Where(d => d.Reclamation.id_DevisionCreate == 6)
+                    .Where(d => d.Reclamation.close == close)
+                    .Count();
+            else if (id_Devision == 3 || id_Devision == 16)
+                count = pZ_PlanZakaz.Reclamation_PZ
+                    .Where(d => d.Reclamation.id_DevisionReclamation == 3 || d.Reclamation.id_DevisionReclamation == 16)
+                    .Where(d => d.Reclamation.close == close)
+                    .Count();
+            else if (id_Devision == 0)
+                count = pZ_PlanZakaz.Reclamation_PZ
+                    .Where(d => d.Reclamation.close == close)
+                    .Count();
+            else
+                count = pZ_PlanZakaz.Reclamation_PZ
+                    .Where(d => d.Reclamation.id_DevisionReclamation == id_Devision)
+                    .Where(d => d.Reclamation.close == close)
+                    .Count();
+            return count;
+        }
     }
 }
