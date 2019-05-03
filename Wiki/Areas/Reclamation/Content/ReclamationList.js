5f5fb045b5b602cd0efea2772311f4edc94e5b3c
Юрий Мельников <yury.melnikau@gmail.com>
2019-05-03 17:25:46 +0300

    Reclamation: - JS CRUD remark

--- Wiki/Areas/Reclamation/Content/ReclamationList.js
+++ Wiki/Areas/Reclamation/Content/ReclamationList.js
@@ -2,7 +2,7 @@
     activeReclamation();
 });
 
-var objRemark = [
+var objRemarksList = [
     { "title": "Ред", "data": "EditLinkJS", "autowidth": true, "bSortable": false },
     { "title": "См", "data": "ViewLinkJS", "autowidth": true, "bSortable": false },
     { "title": "Заказ", "data": "PlanZakaz", "autowidth": true, "bSortable": true },
@@ -17,6 +17,26 @@ var objRemark = [
     { "title": "Степень ошибки", "data": "LeavelReclamation", "autowidth": true, "bSortable": true }
 ];
 
+var objRemark = {
+    id: $('#id').val(),
+    id_Reclamation_Type: $('#id_Reclamation_Type').val(),
+    id_DevisionReclamation: $('#id_DevisionReclamation').val(),
+    id_Reclamation_CountErrorFirst: $('#id_Reclamation_CountErrorFirst').val(),
+    id_Reclamation_CountErrorFinal: $('#id_Reclamation_CountErrorFinal').val(),
+    id_PZ_OperatorDogovora: $('#id_PZ_OperatorDogovora').val(),
+    id_AspNetUsersCreate: $('#id_AspNetUsersCreate').val(),
+    id_DevisionCreate: $('#id_DevisionCreate').val(),
+    dateTimeCreate: $('#dateTimeCreate').val(),
+    text: $('#text').val(),
+    description: $('#description').val(),
+    timeToSearch: $('#timeToSearch').val(),
+    timeToEliminate: $('#timeToEliminate').val(),
+    close: $('#close').val(),
+    closeDevision: $('#closeDevision').val(),
+    PCAM: $('#PCAM').val(),
+    editManufacturing: $('#editManufacturing').val()
+};
+
 function activeReclamation() {
     $("#myTable").DataTable({
         "ajax": {
@@ -28,7 +48,7 @@ function activeReclamation() {
         "bDestroy": true,
         "processing": true,
         "order": [[2, "desc"]],
-        "columns": objRemark,
+        "columns": objRemarksList,
         "scrollY": '75vh',
         "scrollX": true,
         "paging": false,
@@ -71,7 +91,7 @@ function closeReclamation() {
         "bDestroy": true,
         "processing": true,
         "order": [[2, "desc"]],
-        "columns": objRemark,
+        "columns": objRemarksList,
         "scrollY": '75vh',
         "scrollX": true,
         "paging": false,
@@ -99,6 +119,160 @@ function closeReclamation() {
                     select.append('<option value="' + d + '">' + d + '</option>');
                 });
             });
+        }
+    });
+}
+
+function Add() {
+    var res = validate();
+    if (res === false) {
+        return false;
+    }
+    $("#btnAdd").attr('disabled', true);
+    var objRemark;
+    $.ajax({
+        cache: false,
+        url: "/Remarks/Add",
+        data: JSON.stringify(objRemark),
+        type: "POST",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            loadData();
+            $('#orderModal').modal('hide');
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+}
+
+function validate() {
+    var isValid = true;
+    if ($('#id_Reclamation_Type').val().trim() === "") {
+        $('#id_Reclamation_Type').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#id_Reclamation_Type').css('border-color', 'lightgrey');
+    }
+    if ($('#id_DevisionReclamation').val() === null) {
+        $('#id_DevisionReclamation').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#id_DevisionReclamation').css('border-color', 'lightgrey');
+    }
+
+    if ($('#text').val() === null) {
+        $('#text').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#text').css('border-color', 'lightgrey');
+    }
+    return isValid;
+}
+
+function clearTextBox() {
+    $("#btnAdd").attr('disabled', false);
+    $('#id').val("");
+    $('#id_Reclamation_Type').val("");
+    $('#id_DevisionReclamation').val("");
+    $('#id_Reclamation_CountErrorFirst').val("");
+    $('#id_Reclamation_CountErrorFinal').val("");
+    $('#id_AspNetUsersCreate').val("");
+    $('#id_DevisionCreate').val("");
+    $('#dateTimeCreate').val("");
+    $('#text').val("");
+    $('#description').val("");
+    $('#timeToSearch').val("");
+    $('#timeToEliminate').val("");
+    $('#close').val("");
+    $('#gip').val("");
+    $('#closeDevision').val("");
+    $('#PCAM').val("");
+    $('#editManufacturing').val("");
+    $('#btnUpdate').hide();
+    $('#btnAdd').show();
+    $('#name').css('border-color', 'lightgrey');
+    $('#active').css('border-color', 'lightgrey');
+}
+
+function getbyID(Id) {
+    $('#name').css('border-color', 'lightgrey');
+    $('#active').css('border-color', 'lightgrey');
+    $.ajax({
+        cache: false,
+        url: "/Remarks/GetOrder/" + Id,
+        typr: "GET",
+        contentType: "application/json;charset=UTF-8",
+        dataType: "json",
+        success: function (result) {
+            $('#id').val(result.id);
+            $('#id_Reclamation_Type').val(result.id_Reclamation_Type);
+            $('#id_DevisionReclamation').val(result.id_DevisionReclamation);
+            $('#id_Reclamation_CountErrorFirst').val(result.id_Reclamation_CountErrorFirst);
+            $('#id_Reclamation_CountErrorFinal').val(result.id_Reclamation_CountErrorFinal);
+            $('#id_AspNetUsersCreate').val(result.id_AspNetUsersCreate);
+            $('#id_DevisionCreate').val(result.id_DevisionCreate);
+            $('#dateTimeCreate').val(result.dateTimeCreate);
+            $('#text').val(result.text);
+            $('#description').val(result.description);
+            $('#timeToSearch').val(result.timeToSearch);
+            $('#timeToEliminate').val(result.timeToEliminate);
+            $('#close').val(result.close);
+            $('#gip').val(result.gip);
+            $('#closeDevision').val(result.closeDevision);
+            $('#PCAM').val(result.PCAM);
+            $('#editManufacturing').val(result.editManufacturing);
+            $('#orderModal').modal('show');
+            $('#btnUpdate').show();
+            $('#btnAdd').hide();
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+    return false;
+}
+
+function Update() {
+    var res = validate();
+    if (res === false) {
+        return false;
+    }
+    objRemark;
+    $.ajax({
+        cache: false,
+        url: "/Remarks/Update",
+        data: JSON.stringify(typeObj),
+        type: "POST",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            loadData();
+            $('#orderModal').modal('hide');
+            $('#id').val("");
+            $('#id_Reclamation_Type').val("");
+            $('#id_DevisionReclamation').val("");
+            $('#id_Reclamation_CountErrorFirst').val("");
+            $('#id_Reclamation_CountErrorFinal').val("");
+            $('#id_AspNetUsersCreate').val("");
+            $('#id_DevisionCreate').val("");
+            $('#dateTimeCreate').val("");
+            $('#text').val("");
+            $('#description').val("");
+            $('#timeToSearch').val("");
+            $('#timeToEliminate').val("");
+            $('#close').val("");
+            $('#gip').val("");
+            $('#closeDevision').val("");
+            $('#PCAM').val("");
+            $('#editManufacturing').val("");
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
         }
     });
 }
