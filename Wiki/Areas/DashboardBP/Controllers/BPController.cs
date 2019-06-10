250dff99eb84334d23d858ee031db571db397330
Юрий Мельников <yury.melnikau@gmail.com>
2019-06-10 10:53:20 +0300

    BP add controller

---
+++ Wiki/Areas/DashboardBP/Controllers/BPController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Wiki.Areas.DashboardBP.Controllers
+{
+    public class BPController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string RenderUserMenu()
+        {
+            string login = "Войти";
+            try
+            {
+                if (HttpContext.User.Identity.Name != "")
+                    login = HttpContext.User.Identity.Name;
+            }
+            catch
+            {
+                login = "Войти";
+            }
+            return login;
+        }
+    }
+}
---
+++ Wiki/Areas/DashboardBP/DashboardBPAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Wiki.Areas.DashboardBP
+{
+    public class DashboardBPAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "DashboardBP";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "DashboardBP_default",
+                "DashboardBP/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
