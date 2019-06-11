67bc4a9a4bb29b84af109f5d01cf4068df047c31
Юрий Мельников <yury.melnikau@gmail.com>
2019-06-11 11:36:19 +0300

    BP: add class TasksList

---
+++ Wiki/Areas/DashboardBP/Models/TasksList.cs
@@ -0,0 +1,15 @@
+﻿namespace Wiki.Areas.DashboardBP.Models
+{
+    public class TasksList
+    {
+        public bool CreateTask(DashboardBP_ProjectList project)
+        {
+            using (PortalKATEKEntities db = new PortalKATEKEntities())
+            {
+
+            }
+            return true;
+        }
+
+    }
+}
