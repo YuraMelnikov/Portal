f40a75e4a0b08819d26697682bf729cc23a094f6
Юрий Мельников <yury.melnikau@gmail.com>
2019-06-18 15:02:10 +0300

    -

--- Wiki/Areas/DashboardBP/Models/NewBP.cs
+++ Wiki/Areas/DashboardBP/Models/NewBP.cs
@@ -4,11 +4,11 @@
     {
         public NewBP()
         {
-            new State().UpdateBP();
-            //update PZ dateShPlan & ProjectUID
-            new ProjectsList().CreatePZList();
-            new RatePlan().CreateNewRatePlan();
-            new HSSPO().CreateNew();
+            //new State().UpdateBP();
+            ////update PZ dateShPlan & ProjectUID
+            //new ProjectsList().CreatePZList();
+            //new RatePlan().CreateNewRatePlan();
+            new SQLHSSPO().CreateNew();
             new RemainingHSS().CreateNew();
             new HssPlan().CreateNew();
         }
---
+++ Wiki/Areas/DashboardBP/Models/SQLHSSPO.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+
+namespace Wiki.Areas.DashboardBP.Models
+{
+    public class SQLHSSPO
+    {
+        public void CreateNew()
+        {
+            string sqlConnectionString = "Data Source=TSQLSERVER;Initial Catalog=PortalKATEK_test;Integrated Security=True";
+            FileInfo file = new FileInfo(@"C:\Users\myi\source\repos\Portal\Wiki\Areas\DashboardBP\Content\createHSSPO.sql");
+            string script = file.OpenText().ReadToEnd();
+            SqlConnection conn = new SqlConnection(sqlConnectionString);
+            Server server = new Server(new ServerConnection(conn));
+            server.ConnectionContext.ExecuteNonQuery(script);
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            while (sw.ElapsedMilliseconds < 15000)
+            {
+            }
+        }
+    }
+}
