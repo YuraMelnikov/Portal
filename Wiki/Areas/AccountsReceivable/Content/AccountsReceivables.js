eaf4ae96ca66323a0551f17b4f7a6b950a8f2caf
Юрий Мельников <yury.melnikau@gmail.com>
2019-06-25 10:52:24 +0300

    -

--- Wiki/Areas/AccountsReceivable/Content/AccountsReceivables.js
+++ Wiki/Areas/AccountsReceivable/Content/AccountsReceivables.js
@@ -8,25 +8,224 @@
 function loadData(listId) {
     document.getElementById('pageId').innerHTML = listId;
     if (listId === 1 || listId === "1") {
-        loadReport();
+        loadData(1);
     }
     else if (listId === 2 || listId === "2") {
-        loadOS();
+        loadData(2);
+    }
+    else if (listId === 3 || listId === "3") {
+        loadData(3);
+    }
+    else if (listId === 4 || listId === "4") {
+        loadData(4);
+    }
+    else if (listId === 5 || listId === "5") {
+        loadData(5);
     }
     else {
-        loadReport();
+        loadData(1);
     }
 }
 
-var objSmallReport = [
-    { "title": "Позиция", "data": "position", "autowidth": true, "bSortable": true, "class": 'colu-200' },
-    { "title": "Подрядчик", "data": "name", "autowidth": true, "bSortable": true },
-    { "title": "Изг. дн.", "data": "day", "autowidth": true, "bSortable": true, "defaultContent": "", "render": processZero },
-    { "title": "Дата создания", "data": "dateCreate", "autowidth": true, "bSortable": true },
-    { "title": "Дата размещения", "data": "workDateTime", "autowidth": true, "bSortable": true, "defaultContent": "", "render": processNull },
-    { "title": "Дата исполнения", "data": "manufDate", "autowidth": true, "bSortable": true, "defaultContent": "", "render": processNull },
-    { "title": "Дата поступления", "data": "finDate", "autowidth": true, "bSortable": true, "defaultContent": "", "render": processNull },
-    { "title": "№ заявки", "data": "id", "autowidth": true, "bSortable": true },
-    { "title": "Статус", "data": "status", "autowidth": true, "bSortable": true },
-    { "title": "Папка заказа", "data": "folder", "autowidth": true }
+var objTasks = [
+    { "title": "См.", "data": "editLink", "autowidth": true, "bSortable": false },
+    { "title": "Заказ", "data": "order", "autowidth": true, "bSortable": true },
+    { "title": "Задача", "data": "taskName", "autowidth": true, "bSortable": true },
+    { "title": "Заказчик", "data": "client", "autowidth": true, "bSortable": true },
+    { "title": "Исполнитель", "data": "user", "autowidth": true, "bSortable": true },
+    { "title": "Плановая дата", "data": "planDate", "autowidth": true, "bSortable": true, "defaultContent": "", "render": processNull }
 ];
+
+var objEditTasks = [
+    { "title": "Ред.", "data": "editLink", "autowidth": true, "bSortable": false },
+    { "title": "См.", "data": "viewLink", "autowidth": true, "bSortable": false },
+    { "title": "Заказ", "data": "order", "autowidth": true, "bSortable": true },
+    { "title": "Задача", "data": "taskName", "autowidth": true, "bSortable": true },
+    { "title": "Заказчик", "data": "client", "autowidth": true, "bSortable": true },
+    { "title": "Исполнитель", "data": "user", "autowidth": true, "bSortable": true },
+    { "title": "Плановая дата", "data": "planDate", "autowidth": true, "bSortable": true, "defaultContent": "", "render": processNull }
+];
+
+var objTEO = [
+    { "title": "Ред.", "data": "editLink", "autowidth": true, "bSortable": false },
+    { "title": "Заказ", "data": "order", "autowidth": true, "bSortable": true },
+    { "title": "Выручка без НДС", "data": "Rate", "autowidth": true, "className": 'text-right', render: $.fn.dataTable.render.number(',', '.', 2, '') },
+    { "title": "С/С материалов", "data": "SSM", "autowidth": true, "className": 'text-right', render: $.fn.dataTable.render.number(',', '.', 2, '') },
+    { "title": "С/С зп рабочих", "data": "SSR", "autowidth": true, "className": 'text-right', render: $.fn.dataTable.render.number(',', '.', 2, '') },
+    { "title": "Коммерчиские издержки", "data": "IzdKom", "autowidth": true, "className": 'text-right', render: $.fn.dataTable.render.number(',', '.', 2, '') },
+    { "title": "% по кредиту", "data": "IzdPPKredit", "autowidth": true, "className": 'text-right', render: $.fn.dataTable.render.number(',', '.', 2, '') },
+    { "title": "Постоянные издержки", "data": "PI", "autowidth": true, "className": 'text-right', render: $.fn.dataTable.render.number(',', '.', 2, '') },
+    { "title": "НОП", "data": "NOP", "autowidth": true, "className": 'text-right', render: $.fn.dataTable.render.number(',', '.', 2, '') },
+    { "title": "КИ Прочие_Ш", "data": "KI_S", "autowidth": true, "className": 'text-right', render: $.fn.dataTable.render.number(',', '.', 2, '') },
+    { "title": "КИ Прочие", "data": "KI_prochee", "autowidth": true, "className": 'text-right', render: $.fn.dataTable.render.number(',', '.', 2, '') },
+    { "title": "Отпускная цена без НДС", "data": "OtpuskChena", "autowidth": true, "className": 'text-right', render: $.fn.dataTable.render.number(',', '.', 2, '') },
+    { "title": "Валюта", "data": "Currency", "autowidth": true, "bSortable": true },
+    { "title": "Сумма НДС", "data": "NDS", "autowidth": true, "className": 'text-right', render: $.fn.dataTable.render.number(',', '.', 2, '') }
+];
+
+var objContract = [
+    { "title": "Номер", "data": "PlanZakaz", "autowidth": true }
+    , { "title": "Менеджер", "data": "Manager", "autowidth": true }
+    , { "title": "Заказчик", "data": "Client", "autowidth": true }
+    , { "title": "Кол-во дней на приемку", "data": "KolVoDneyNaPrijemku", "autowidth": true }
+    , { "title": "Условия приемки изделия", "data": "PunktDogovoraOSrokahPriemki", "autowidth": true }
+    , { "title": "Условия оплаты", "data": "UslovieOplatyText", "autowidth": true }
+];
+
+function startMenu() {
+    $("#tableData").DataTable({
+        "ajax": {
+            "cache": false,
+            "url": "/AccountsReceivables/TasksList",
+            "type": "POST",
+            "datatype": "json"
+        },
+        "order": [[1, "asc"]],
+        "processing": true,
+        "columns": objTasks,
+        "scrollY": '75vh',
+        "scrollX": true,
+        "paging": false,
+        "info": false,
+        "scrollCollapse": true,
+        "language": {
+            "zeroRecords": "Отсутствуют записи",
+            "infoEmpty": "Отсутствуют записи",
+            "search": "Поиск"
+        }
+    });
+}
+
+function tasksList() {
+    var table = $('#tableData').DataTable();
+    table.destroy();
+    $('#tableData').empty();
+    $("#tableData").DataTable({
+        "ajax": {
+            "cache": false,
+            "url": "/AccountsReceivables/TasksList",
+            "type": "POST",
+            "datatype": "json"
+        },
+        "bDestroy": true,
+        "order": [[1, "asc"]],
+        "processing": true,
+        "columns": objTasks,
+        "scrollY": '75vh',
+        "scrollX": true,
+        "paging": false,
+        "info": false,
+        "scrollCollapse": true,
+        "language": {
+            "zeroRecords": "Отсутствуют записи",
+            "infoEmpty": "Отсутствуют записи",
+            "search": "Поиск"
+        }
+    });
+}
+
+function myTasksList() {
+    var table = $('#tableData').DataTable();
+    table.destroy();
+    $('#tableData').empty();
+    $("#tableData").DataTable({
+        "ajax": {
+            "cache": false,
+            "url": "/AccountsReceivables/MyTasksList",
+            "type": "POST",
+            "datatype": "json"
+        },
+        "bDestroy": true,
+        "order": [[1, "asc"]],
+        "processing": true,
+        "columns": objTasks,
+        "scrollY": '75vh',
+        "scrollX": true,
+        "paging": false,
+        "info": false,
+        "scrollCollapse": true,
+        "language": {
+            "zeroRecords": "Отсутствуют записи",
+            "infoEmpty": "Отсутствуют записи",
+            "search": "Поиск"
+        }
+    });
+}
+
+function TEOList() {
+    var table = $('#tableData').DataTable();
+    table.destroy();
+    $('#tableData').empty();
+    $("#tableData").DataTable({
+        "ajax": {
+            "cache": false,
+            "url": "/AccountsReceivables/TEOList",
+            "type": "POST",
+            "datatype": "json"
+        },
+        "bDestroy": true,
+        "order": [[1, "asc"]],
+        "processing": true,
+        "columns": objTEO,
+        "scrollY": '75vh',
+        "scrollX": true,
+        "paging": false,
+        "info": false,
+        "scrollCollapse": true,
+        "language": {
+            "zeroRecords": "Отсутствуют записи",
+            "infoEmpty": "Отсутствуют записи",
+            "search": "Поиск"
+        }
+    });
+}
+
+function contractList() {
+    var table = $('#tableData').DataTable();
+    table.destroy();
+    $('#tableData').empty();
+    $("#tableData").DataTable({
+        "ajax": {
+            "cache": false,
+            "url": "/AccountsReceivables/ContractList",
+            "type": "POST",
+            "datatype": "json"
+        },
+        "bDestroy": true,
+        "order": [[1, "asc"]],
+        "processing": true,
+        "columns": objContract,
+        "scrollY": '75vh',
+        "scrollX": true,
+        "paging": false,
+        "info": false,
+        "scrollCollapse": true,
+        "language": {
+            "zeroRecords": "Отсутствуют записи",
+            "infoEmpty": "Отсутствуют записи",
+            "search": "Поиск"
+        }
+    });
+}
+
+function processNull(data) {
+    if (data === 'null') {
+        return '';
+    } else {
+        return data;
+    }
+}
+
+//getTEO
+//getTask
+
+//close1OpenInSystem
+//close3Prototype
+
+
+
+
+//close2TEO
+//close4ContractSetting
+
+
--- Wiki/Areas/AccountsReceivable/Controllers/AccountsReceivablesController.cs
+++ Wiki/Areas/AccountsReceivable/Controllers/AccountsReceivablesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,6 +12,125 @@ namespace Wiki.Areas.AccountsReceivable.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult TEOList()
+        {
+            using (PortalKATEKEntities db = new PortalKATEKEntities())
+            {
+                var query = db.PZ_TEO
+                    .AsNoTracking()
+                    .Include(d => d.PZ_PlanZakaz)
+                    .Include(d => d.PZ_Currency)
+                    .OrderByDescending(d => d.PZ_PlanZakaz.PlanZakaz)
+                    .ToList();
+                var data = query.Select(dataList => new
+                {
+                    editLink = "<td><a href=" + '\u0022' + "#" + '\u0022' + " onclick=" + '\u0022' + "return getTEO('" + dataList.id + "')" + '\u0022' + "><span class=" + '\u0022' + "glyphicon glyphicon-pencil" + '\u0022' + "></span></a></td>",
+                    order = dataList.PZ_PlanZakaz.PlanZakaz,
+                    dataList.Rate,
+                    dataList.SSM,
+                    dataList.SSR,
+                    dataList.IzdKom,
+                    dataList.IzdPPKredit,
+                    dataList.PI,
+                    dataList.NOP,
+                    dataList.KI_S,
+                    dataList.KI_prochee,
+                    dataList.OtpuskChena,
+                    Currency = dataList.PZ_Currency.Name,
+                    dataList.NDS
+                });
+                return Json(new { data });
+            }
+        }
+
+        public JsonResult TasksList()
+        {
+            using (PortalKATEKEntities db = new PortalKATEKEntities())
+            {
+                var query = db.Debit_WorkBit
+                    .AsNoTracking()
+                    .Include(d => d.TaskForPZ.AspNetUsers)
+                    .Include(d => d.PZ_PlanZakaz.PZ_Client)
+                    .Where(d => d.close == false)
+                    .OrderBy(d => d.PZ_PlanZakaz.PlanZakaz)
+                    .ToList();
+                var data = query.Select(dataList => new
+                {
+                    editLink = "<td><a href=" + '\u0022' + "#" + '\u0022' + " onclick=" + '\u0022' + "return getTask('" + dataList.id + "')" + '\u0022' + "><span class=" + '\u0022' + "glyphicon glyphicon-list-alt" + '\u0022' + "></span></a></td>",
+                    order = dataList.PZ_PlanZakaz.PlanZakaz,
+                    dataList.TaskForPZ.taskName,
+                    client = dataList.PZ_PlanZakaz.PZ_Client.NameSort,
+                    user = dataList.TaskForPZ.AspNetUsers.CiliricalName,
+                    planDate = dataList.datePlan.ToShortDateString()
+                });
+                return Json(new { data });
+            }
+        }
+
+        public JsonResult MyTasksList()
+        {
+            using (PortalKATEKEntities db = new PortalKATEKEntities())
+            {
+                var query = db.Debit_WorkBit
+                    .AsNoTracking()
+                    .Include(d => d.TaskForPZ.AspNetUsers)
+                    .Include(d => d.PZ_PlanZakaz.PZ_Client)
+                    .Where(d => d.close == false)
+                    .OrderBy(d => d.PZ_PlanZakaz.PlanZakaz)
+                    .ToList();
+                var data = query.Select(dataList => new
+                {
+                    editLink = "<td><a href=" + '\u0022' + "#" + '\u0022' + " onclick=" + '\u0022' + "return getTask('" + dataList.id + "')" + '\u0022' + "><span class=" + '\u0022' + "glyphicon glyphicon-pencil" + '\u0022' + "></span></a></td>",
+                    viewLink = "<td><a href=" + '\u0022' + "#" + '\u0022' + " onclick=" + '\u0022' + "return getTask('" + dataList.id + "')" + '\u0022' + "><span class=" + '\u0022' + "glyphicon glyphicon-list-alt" + '\u0022' + "></span></a></td>",
+                    order = dataList.PZ_PlanZakaz.PlanZakaz,
+                    dataList.TaskForPZ.taskName,
+                    client = dataList.PZ_PlanZakaz.PZ_Client.NameSort,
+                    user = dataList.TaskForPZ.AspNetUsers.CiliricalName,
+                    planDate = dataList.datePlan.ToShortDateString()
+                });
+                return Json(new { data });
+            }
+        }
+
+        public JsonResult ContractList()
+        {
+            using (PortalKATEKEntities db = new PortalKATEKEntities())
+            {
+                var query = db.PZ_Setup
+                    .AsNoTracking()
+                    .Include(d => d.PZ_PlanZakaz.PZ_Client)
+                    .Include(d => d.PZ_PlanZakaz.AspNetUsers)
+                    .Where(d => d.PZ_PlanZakaz.DateCreate.Year > 2017)
+                    .Where(d => d.PZ_PlanZakaz.Client != 39)
+                    .ToList();
+                var data = query.Select(dataList => new
+                {
+                    dataList.PZ_PlanZakaz.PlanZakaz,
+                    Manager = dataList.PZ_PlanZakaz.AspNetUsers.CiliricalName,
+                    Client = dataList.PZ_PlanZakaz.PZ_Client.NameSort,
+                    dataList.KolVoDneyNaPrijemku,
+                    dataList.PunktDogovoraOSrokahPriemki,
+                    dataList.UslovieOplatyText
+                });
+                return Json(new { data });
+            }
+        }
+
+        public string RenderUserMenu()
+        {
+            string login = "Войти";
+            try
+            {
+                if (HttpContext.User.Identity.Name != "")
+                    login = HttpContext.User.Identity.Name;
+            }
+            catch
+            {
+                login = "Войти";
+            }
+            return login;
         }
     }
 }
