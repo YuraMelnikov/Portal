17519c4b3c8cd3ab74caf27fafc1e04a31d28a45
YuraMelnikov <yury.melnikau@gmail.com>
2019-08-01 14:36:28 +0300

    -

--- Wiki/Areas/Cells/Content/ScellsJS.js
+++ Wiki/Areas/Cells/Content/ScellsJS.js
@@ -4,23 +4,150 @@
 
 function loadData(listId) {
     if (listId === 1 || listId === "1") {
-        activeReclamation();
+        list();
     }
     else {
-        activeReclamation();
+        list();
     }
 }
 
-var objRemarksList = [
-    { "title": "№", "data": "Id_Reclamation", "autowidth": true, "bSortable": true },
-    { "title": "Ред", "data": "EditLinkJS", "autowidth": true, "bSortable": false },
-    { "title": "См", "data": "ViewLinkJS", "autowidth": true, "bSortable": false },
-    { "title": "Заказ", "data": "PlanZakaz", "autowidth": true, "bSortable": true },
-    { "title": "СП", "data": "Devision", "autowidth": true, "bSortable": true },
-    { "title": "Описание", "data": "Text", "autowidth": true, "bSortable": false, "class": 'colu-200' },
-    { "title": "Прим.", "data": "Description", "autowidth": true, "bSortable": false },
-    { "title": "Ответ/ы", "data": "Answers", "autowidth": true, "bSortable": false, "class": 'colu-200' },
-    { "title": "Создал", "data": "UserCreate", "autowidth": true, "bSortable": true, "class": 'colu-200' },
-    { "title": "Ответственный", "data": "UserReclamation", "autowidth": true, "bSortable": true },
-    { "title": "Степень ошибки", "data": "LeavelReclamation", "autowidth": true, "bSortable": true }
+var objList = [
+    { "title": "ИД. начальной секции", "data": "sectionIdStart", "autowidth": true, "bSortable": true },
+    { "title": "Наименование начальной секции", "data": "name1", "autowidth": true, "bSortable": true },
+    { "title": "ИД. конечной секции", "data": "sectionIdFinish", "autowidth": true, "bSortable": true },
+    { "title": "Наименование конечной секции", "data": "name2", "autowidth": true, "bSortable": true },
+    { "title": "Расстояние (м)", "data": "distance", "autowidth": true, "bSortable": true }
 ];
+
+function startMenu() {
+    $("#myTable").DataTable({
+        "ajax": {
+            "cache": false,
+            "url": "/SCells/List",
+            "type": "POST",
+            "datatype": "json"
+        },
+        "bDestroy": true,
+        "order": [[0, "asc"]],
+        "processing": true,
+        "columns": objList,
+        "rowCallback": function (row, data, index) {
+            if (data.distance === 0 && data.distance === "0") {
+                $('td', row).css('background-color', '#d9534f');
+                $('td', row).css('color', 'white');
+            }
+        },
+        "scrollY": '75vh',
+        "scrollX": true,
+        "paging": false,
+        "info": false,
+        "scrollCollapse": true,
+        "language": {
+            "zeroRecords": "Отсутствуют записи",
+            "infoEmpty": "Отсутствуют записи",
+            "search": "Поиск"
+        }
+    });
+}
+
+function list() {
+    var table = $('#myTable').DataTable();
+    table.destroy();
+    $('#myTable').empty();
+    $("#myTable").DataTable({
+        "ajax": {
+            "cache": false,
+            "url": "/SCells/List",
+            "type": "POST",
+            "datatype": "json"
+        },
+        "bDestroy": true,
+        "order": [[0, "asc"]],
+        "processing": true,
+        "columns": objList,
+        "rowCallback": function (row, data, index) {
+            if (data.distance === 0 && data.distance === "0") {
+                $('td', row).css('background-color', '#d9534f');
+                $('td', row).css('color', 'white');
+            }
+        },
+        "scrollY": '75vh',
+        "scrollX": true,
+        "paging": false,
+        "info": false,
+        "scrollCollapse": true,
+        "language": {
+            "zeroRecords": "Отсутствуют записи",
+            "infoEmpty": "Отсутствуют записи",
+            "search": "Поиск"
+        }
+    });
+}
+
+function getPoint(id) {
+    $.ajax({
+        cache: false,
+        url: "/SCells/GetPoint/" + id,
+        typr: "GET",
+        contentType: "application/json;charset=UTF-8",
+        dataType: "json",
+        success: function (result) {
+            $('#distance').css('border-color', 'lightgrey');
+            $('#id').val("");
+            $('#sectionIdStart').val("");
+            $('#name1').val("");
+            $('#sectionIdFinish').val("");
+            $('#name2').val("");
+            $('#distance').val("");
+            $('#id').val(result.id);
+            $('#sectionIdStart').val(result.sectionIdStart);
+            $('#name1').val(result.name1);
+            $('#sectionIdFinish').val(result.sectionIdFinish);
+            $('#name2').val(result.name2);
+            $('#distance').val(result.distance);
+            $('#pointModal').modal('show');
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+    return false;
+}
+
+function updatePoint() {
+    var res = valid();
+    if (res === false) {
+        return false;
+    }
+    var objPoint = {
+        id: $('#id').val()
+        ,distance: $('#distance').val().replace(",", ".")
+    };
+    $.ajax({
+        cache: false,
+        url: "/SCells/UpdatePoint/",
+        data: JSON.stringify(objPoint),
+        type: "POST",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            $('#myTable').DataTable().ajax.reload(null, false);
+            $('#pointModal').modal('hide');
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+}
+
+function valid() {
+    var isValid = true;
+    if ($('#distance').val().trim() === "") {
+        $('#distance').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#distance').css('border-color', 'lightgrey');
+    }
+    return isValid;
+}
