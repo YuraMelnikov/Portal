729feefe3b477f3f4fba0c9e60fc1a1011fdcb8f
YuraMelnikov <yury.melnikau@gmail.com>
2019-08-01 14:18:21 +0300

    -

---
+++ Wiki/Areas/Cells/Content/ScellsJS.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    startMenu();
+});
+
+function loadData(listId) {
+    if (listId === 1 || listId === "1") {
+        activeReclamation();
+    }
+    else {
+        activeReclamation();
+    }
+}
+
+var objRemarksList = [
+    { "title": "№", "data": "Id_Reclamation", "autowidth": true, "bSortable": true },
+    { "title": "Ред", "data": "EditLinkJS", "autowidth": true, "bSortable": false },
+    { "title": "См", "data": "ViewLinkJS", "autowidth": true, "bSortable": false },
+    { "title": "Заказ", "data": "PlanZakaz", "autowidth": true, "bSortable": true },
+    { "title": "СП", "data": "Devision", "autowidth": true, "bSortable": true },
+    { "title": "Описание", "data": "Text", "autowidth": true, "bSortable": false, "class": 'colu-200' },
+    { "title": "Прим.", "data": "Description", "autowidth": true, "bSortable": false },
+    { "title": "Ответ/ы", "data": "Answers", "autowidth": true, "bSortable": false, "class": 'colu-200' },
+    { "title": "Создал", "data": "UserCreate", "autowidth": true, "bSortable": true, "class": 'colu-200' },
+    { "title": "Ответственный", "data": "UserReclamation", "autowidth": true, "bSortable": true },
+    { "title": "Степень ошибки", "data": "LeavelReclamation", "autowidth": true, "bSortable": true }
+];
---
+++ Wiki/Areas/Cells/Controllers/SCellsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Wiki.Areas.Cells.Controllers
+{
+    public class SCellsController : Controller
+    {
+        public ActionResult Index()
+        {
+            using (PortalKATEKEntities db = new PortalKATEKEntities())
+            {
+
+            }
+            return View();
+        }
+    }
+}
