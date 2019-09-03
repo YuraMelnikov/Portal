9d074384715f64632bbe91efb4bed46c8a4b3d0d
YuraMelnikov <yury.melnikau@gmail.com>
2019-09-03 16:17:41 +0300

    -

---
+++ Wiki/Areas/DashboardKO/DashboardKOAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Wiki.Areas.DashboardKO
+{
+    public class DashboardKOAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "DashboardKO";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "DashboardKO_default",
+                "DashboardKO/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
