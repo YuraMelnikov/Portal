c320fb0a74e88cc1b1ac486b6997659f89ef73b3
YuraMelnikov <yury.melnikau@gmail.com>
2019-09-03 16:23:10 +0300

    -

--- Wiki/Areas/DashboardKO/Controllers/ReportPageController.cs
+++ Wiki/Areas/DashboardKO/Controllers/ReportPageController.cs
@@ -1,13 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Web.Mvc;
+using Wiki.Areas.DashboardBP.Models;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System;
+using System.Web.Script.Serialization;
 
 namespace Wiki.Areas.DashboardKO.Controllers
 {
     public class ReportPageController : Controller
     {
+        readonly JsonSerializerSettings shortDefaultSetting = new JsonSerializerSettings { DateFormatString = "dd.MM.yyyy" };
+        JavaScriptSerializer js = new JavaScriptSerializer();
+
         public ActionResult Index()
         {
             return View();
