c2fc7c85a317a9db56ce1d4288a877f4ef4cc641
YuraMelnikov <yury.melnikau@gmail.com>
2019-09-12 17:23:20 +0300

    -

---
+++ Wiki/Areas/UsersKO/Controllers/UserKOController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Wiki.Areas.UsersKO.Controllers
+{
+    public class UserKOController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Wiki/Areas/UsersKO/UsersKOAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Wiki.Areas.UsersKO
+{
+    public class UsersKOAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "UsersKO";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "UsersKO_default",
+                "UsersKO/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
