229dec7bea1e45569e2603e8b329f5582b9ebb04
YuraMelnikov <yury.melnikau@gmail.com>
2019-09-16 15:18:14 +0300

    -

---
+++ Wiki/Areas/DashboardTV/Controllers/DashboardTVController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Wiki.Areas.DashboardTV.Controllers
+{
+    public class DashboardTVController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Wiki/Areas/DashboardTV/DashboardTVAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Wiki.Areas.DashboardTV
+{
+    public class DashboardTVAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "DashboardTV";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "DashboardTV_default",
+                "DashboardTV/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
