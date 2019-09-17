b917ed09393d2ae958eff114897590bbfa254e63
YuraMelnikov <yury.melnikau@gmail.com>
2019-09-17 15:28:06 +0300

    -

---
+++ Wiki/Areas/DashboardTV/Models/DealsForDashboardTV.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Wiki.Areas.DashboardTV.Models
+{
+    public class DealsForDashboardTV
+    {
+        int tcpm;
+        DateTime from;
+        DateTime to;
+
+        public int TCPM { get => tcpm; set => tcpm = value; }
+        public DateTime From { get => from; set => from = value; }
+        public DateTime To { get => to; set => to = value; }
+    }
+}
---
+++ Wiki/Areas/DashboardTV/Models/OrderForDashboardTV.cs
@@ -0,0 +1,13 @@
+﻿namespace Wiki.Areas.DashboardTV.Models
+{
+    public class OrderForDashboardTV
+    {
+        string orderNumber;
+        int current;
+        DealsForDashboardTV[] deals;
+
+        public string OrderNumber { get => orderNumber; set => orderNumber = value; }
+        public int Current { get => current; set => current = value; }
+        public DealsForDashboardTV[] Deals { get => deals; set => deals = value; }
+    }
+}
