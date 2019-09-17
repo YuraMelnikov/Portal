84796d16d83b140d7ab38639c5240c012be4bc3c
YuraMelnikov <yury.melnikau@gmail.com>
2019-09-17 09:51:40 +0300

    -

--- Wiki/Areas/DashboardTV/Content/DashboardTV.js
+++ Wiki/Areas/DashboardTV/Content/DashboardTV.js
@@ -1,3 +1,17 @@
 ﻿$(document).ready(function () {
-
+    getPeriodReport();
 });
+
+function getPeriodReport() {
+    $.ajax({
+        url: "/BP/GetPeriodReport/",
+        contentType: "application/json;charset=UTF-8",
+        dataType: "json",
+        success: function (result) {
+            document.getElementById("periodReportString").textContent = result;
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+}
