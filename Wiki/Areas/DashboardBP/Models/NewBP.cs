97b0f98e4ec5b9cd389f4d1e0f8656631c247564
YuraMelnikov <yury.melnikau@gmail.com>
2019-10-08 15:19:38 +0300

    -

--- Wiki/Areas/DashboardBP/Models/NewBP.cs
+++ Wiki/Areas/DashboardBP/Models/NewBP.cs
@@ -4,11 +4,8 @@
     {
         public NewBP()
         {
-            new State().UpdateBP();
-            //update PZ dateShPlan & ProjectUID
             new ProjectsList().CreatePZList();
             new RatePlan().CreateNewRatePlan();
-            new SQLHSSPO().CreateNew();
             new RemainingHSS().CreateNew();
             new HssPlan().CreateNew();
         }
--- Wiki/Areas/VisualizationBP/Content/VBP.js
+++ Wiki/Areas/VisualizationBP/Content/VBP.js
@@ -1,1 +1,200 @@
-﻿
+﻿$(document).ready(function () {
+    //getGanttProjectPortfolio();
+    getPeriodReport();
+    getHSSPlanToYear();
+    getRemainingHss();
+    getRetePlan();
+    getHSSToDay();
+});
+
+function getPeriodReport() {
+    $.ajax({
+        url: "/BP/GetPeriodReport/",
+        contentType: "application/json;charset=UTF-8",
+        dataType: "json",
+        success: function (result) {
+            document.getElementById("periodReportString").textContent = result;
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+}
+
+function getGanttProjectPortfolio() {
+    $.ajax({
+        url: "/BP/GetProjectsPortfolio/",
+        contentType: "application/json;charset=UTF-8",
+        dataType: "json",
+        success: function (result) {
+            var arrayToJSON = result.projects;
+            var myJSON = JSON.parse(JSON.stringify(arrayToJSON));
+            var countLine = myJSON.length;
+            Highcharts.setOptions({
+                lang: {
+                    loading: 'Загрузка...',
+                    months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+                    weekdays: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+                    shortMonths: ['Янв', 'Фев', 'Март', 'Апр', 'Май', 'Июнь', 'Июль', 'Авг', 'Сент', 'Окт', 'Нояб', 'Дек'],
+                    exportButtonTitle: "Экспорт",
+                    printButtonTitle: "Печать",
+                    rangeSelectorFrom: "С",
+                    rangeSelectorTo: "По",
+                    rangeSelectorZoom: "Период",
+                    downloadPNG: 'Скачать PNG',
+                    downloadJPEG: 'Скачать JPEG',
+                    downloadPDF: 'Скачать PDF',
+                    downloadSVG: 'Скачать SVG',
+                    printChart: 'Напечатать график',
+                    Week: 'Нед.',
+                    Start: 'Начало'
+                },
+                credits: {
+                    enabled: false
+                }
+            });
+            var today = new Date(),
+                day = 1000 * 60 * 60 * 24,
+                dateFormat = Highcharts.dateFormat,
+                defined = Highcharts.defined,
+                isObject = Highcharts.isObject,
+                reduce = Highcharts.reduce;
+            today.setUTCHours(0);
+            today.setUTCMinutes(0);
+            today.setUTCSeconds(0);
+            today.setUTCMilliseconds(0);
+            today = today.getTime();
+            Highcharts.ganttChart('projectPortfolio', {
+                xAxis: {
+                    scrollbar: {
+                        enabled: true
+                    },
+                    currentDateIndicator: true,
+                    min: today - 7 * day,
+                    max: today + 120 * day,
+                    labels: {
+                        style: {
+                            "color": "#666666",
+                            "fontSize": "13px"
+                        }
+                    }
+                },
+                chart: {
+                    height: '900px'
+                },
+                series: [{
+                    pointWidth: 13,
+                    name: 'Заказ',
+                    data: myJSON
+                }],
+                yAxis: {
+                    type: 'category',
+                    grid: {
+                        scrollbar: {
+                            enabled: false
+                        },
+                        columns: [{
+                            title: {
+                                text: 'Заказ'
+                            },
+                            labels: {
+                                format: '{point.name}'
+                            },
+                            scrollbar: {
+                                enabled: false
+                            }
+                        }, {
+                            title: {
+                                text: 'Контракт'
+                            },
+                            labels: {
+                                format: '{point.contractDate:%e. %b}'
+                            },
+                            scrollbar: {
+                                enabled: false
+                            }
+
+                        }, {
+                            title: {
+                                text: 'План'
+                            },
+                            labels: {
+                                format: '{point.end:%e. %b}'
+                            },
+                            scrollbar: {
+                                enabled: false
+                            }
+                        }, {
+                            title: {
+                                text: 'Откл.'
+                            },
+                            labels: {
+                                formatter: function () {
+                                    var point = this.point,
+                                        days = 1000 * 60 * 60 * 24,
+                                        number = (point.contractDate - point.end) / days;
+                                    if (Math.round(number * 100) / 100 < 0) {
+                                        return '<span style="fill: red; font-weight:bold;">' + Math.round(number * 100) / 100 + '</span>';
+                                    } else {
+                                        return Math.round(number * 100) / 100;
+                                    }
+                                }
+                            }
+                        }]
+                    },
+                    scrollbar: {
+                        enabled: true
+                    },
+                    min: 0,
+                    max: 40,
+                    labels: {
+                        style: {
+                            "color": "#666666",
+                            "fontSize": "10px"
+                        }
+                    }
+                },
+                tooltip: {
+                    pointFormatter: function () {
+                        var point = this,
+                            format = '%e. %b',
+                            options = point.options,
+                            completed = options.completed,
+                            amount = isObject(completed) ? completed.amount : completed,
+                            status = (amount || 0) * 100 + '%',
+                            lines;
+                        lines = [{
+                            value: point.name,
+                            style: 'font-weight: bold;'
+                        }, {
+                            title: 'Начало',
+                            value: dateFormat(format, point.start)
+                        }, {
+                            visible: !options.milestone,
+                            title: 'Окончание',
+                            value: dateFormat(format, point.end)
+                        }, {
+                            visible: !options.milestone,
+                            title: 'Контрактный срок',
+                            value: dateFormat(format, point.contractDate)
+                        }, {
+                            title: 'Владелец',
+                            value: options.owner || 'unassigned'
+                        }];
+                        return reduce(lines, function (str, line) {
+                            var s = '',
+                                style = defined(line.style) ? line.style : 'font-size: 0.8em;';
+                            if (line.visible !== false) {
+                                s = '<span style="' + style + '">' + (defined(line.title) ? line.title + ': ' : '') + (defined(line.value) ? line.value : '') + '</span><br/>';
+                            }
+                            return str + s;
+                        }, '');
+                    }
+                }
+            });
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+}
