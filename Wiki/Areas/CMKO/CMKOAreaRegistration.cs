1356cf06b264366fc4e12157d7fe820cdd133549
YuraMelnikov <yury.melnikau@gmail.com>
2019-10-18 12:07:33 +0300

    -

---
+++ Wiki/Areas/CMKO/CMKOAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Wiki.Areas.CMKO
+{
+    public class CMKOAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "CMKO";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "CMKO_default",
+                "CMKO/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Wiki/Areas/CMKO/Controllers/CMKController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Wiki.Areas.CMKO.Controllers
+{
+    public class CMKController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
