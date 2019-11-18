78b350ecdc3356bf245d5a59e0cdc587f6d21945
YuraMelnikov <yury.melnikau@gmail.com>
2019-11-18 14:57:45 +0300

    -

--- Wiki/SQL Scripts/StartFinishWorkForSDI.sql
+++ Wiki/SQL Scripts/StartFinishWorkForSDI.sql
@@ -1,5 +1,13 @@
 select
-*
+ManufacturingTable.[№заказа],
+ManufacturingTable.[Начало производства],
+ManufacturingTable.[Окончание производства],
+KBMStartTable.[Начало разработки] as [Начало разработки КБМ],
+KBMFinishTable.[Окончание разработки] as [Окончание разработки КБМ],
+KBEStartTable.[Начало разработки] as [Начало разработки КБЭ],
+KBEFinishTable.[Окончание разработки] as [Окончание разработки КБЭ],
+StartWorkToRKDTable.[Дата готовности первой версии РКД],
+ComplitedRKDTable.[Дата согласования РКД]
 From
 (SELECT MSP_EpmProject_UserView.[№ заказа] as [№заказа], min(MSP_EpmAssignmentByDay_UserView.TimeByDay) as [Начало производства], max(MSP_EpmAssignmentByDay_UserView.TimeByDay) as [Окончание производства]
 	FROM ProjectWebApp.dbo.MSP_EpmProject_UserView INNER JOIN ProjectWebApp.dbo.MSP_EpmTask_UserView ON
@@ -15,12 +23,11 @@ From
 	ProjectWebApp.dbo.MSP_EpmResource_UserView.СДРес like '%УСМК%' or 
 	ProjectWebApp.dbo.MSP_EpmResource_UserView.СДРес like '%УСС%' or 
 	ProjectWebApp.dbo.MSP_EpmResource_UserView.СДРес like '%УСР%' or 
-	ProjectWebApp.dbo.MSP_EpmResource_UserView.СДРес like '%Уиш%')
-group by MSP_EpmProject_UserView.[№ заказа]) as ManufacturingTable
-
-select
-*
-From
+	ProjectWebApp.dbo.MSP_EpmResource_UserView.СДРес like '%Уиш%') 
+	and MSP_EpmProject_UserView.[№ заказа] not like '%НИОКР%'
+	and MSP_EpmProject_UserView.[№ заказа] not like '%Прочие%'
+	and MSP_EpmProject_UserView.[№ заказа] not like '%Задан%'
+	group by MSP_EpmProject_UserView.[№ заказа]) as ManufacturingTable left join
 (SELECT MSP_EpmProject_UserView.[№ заказа] as [№заказа], min(MSP_EpmAssignmentByDay_UserView.TimeByDay) as [Начало разработки]
 	FROM ProjectWebApp.dbo.MSP_EpmProject_UserView INNER JOIN ProjectWebApp.dbo.MSP_EpmTask_UserView ON
 	ProjectWebApp.dbo.MSP_EpmProject_UserView.ProjectUID = ProjectWebApp.dbo.MSP_EpmTask_UserView.ProjectUID LEFT OUTER JOIN ProjectWebApp.dbo.MSP_EpmAssignmentByDay_UserView
@@ -32,11 +39,7 @@ From
 	WHERE
 	(ProjectWebApp.dbo.MSP_EpmAssignmentByDay_UserView.AssignmentActualWork > 0) AND
 	(ProjectWebApp.dbo.MSP_EpmResource_UserView.СДРес like '%КБМ%')
-group by MSP_EpmProject_UserView.[№ заказа]) as KBMStartTable
-
-select
-*
-From
+	group by MSP_EpmProject_UserView.[№ заказа]) as KBMStartTable on ManufacturingTable.[№заказа] = KBMStartTable.[№заказа] left join
 (SELECT MSP_EpmProject_UserView.[№ заказа] as [№заказа], max(MSP_EpmAssignmentByDay_UserView.TimeByDay) as [Окончание разработки]
 	FROM ProjectWebApp.dbo.MSP_EpmProject_UserView INNER JOIN ProjectWebApp.dbo.MSP_EpmTask_UserView ON
 	ProjectWebApp.dbo.MSP_EpmProject_UserView.ProjectUID = ProjectWebApp.dbo.MSP_EpmTask_UserView.ProjectUID LEFT OUTER JOIN ProjectWebApp.dbo.MSP_EpmAssignmentByDay_UserView
@@ -48,11 +51,7 @@ From
 	WHERE
 	(ProjectWebApp.dbo.MSP_EpmAssignmentByDay_UserView.AssignmentActualWork > 0) AND
 	(ProjectWebApp.dbo.MSP_EpmResource_UserView.СДРес like '%КБМ%' and ProjectWebApp.dbo.MSP_EpmTask_UserView.TaskName like '%ведомость%')
-group by MSP_EpmProject_UserView.[№ заказа]) as KBMFinishTable
-
-select
-*
-From
+	group by MSP_EpmProject_UserView.[№ заказа]) as KBMFinishTable on ManufacturingTable.[№заказа] = KBMFinishTable.[№заказа] left join
 (SELECT MSP_EpmProject_UserView.[№ заказа] as [№заказа], min(MSP_EpmAssignmentByDay_UserView.TimeByDay) as [Начало разработки]
 	FROM ProjectWebApp.dbo.MSP_EpmProject_UserView INNER JOIN ProjectWebApp.dbo.MSP_EpmTask_UserView ON
 	ProjectWebApp.dbo.MSP_EpmProject_UserView.ProjectUID = ProjectWebApp.dbo.MSP_EpmTask_UserView.ProjectUID LEFT OUTER JOIN ProjectWebApp.dbo.MSP_EpmAssignmentByDay_UserView
@@ -64,11 +63,7 @@ From
 	WHERE
 	(ProjectWebApp.dbo.MSP_EpmAssignmentByDay_UserView.AssignmentActualWork > 0) AND
 	(ProjectWebApp.dbo.MSP_EpmResource_UserView.СДРес like '%КБЭ%')
-group by MSP_EpmProject_UserView.[№ заказа]) as KBEStartTable
-
-select
-*
-From
+	group by MSP_EpmProject_UserView.[№ заказа]) as KBEStartTable on ManufacturingTable.[№заказа] = KBEStartTable.[№заказа] left join
 (SELECT MSP_EpmProject_UserView.[№ заказа] as [№заказа], max(MSP_EpmAssignmentByDay_UserView.TimeByDay) as [Окончание разработки]
 	FROM ProjectWebApp.dbo.MSP_EpmProject_UserView INNER JOIN ProjectWebApp.dbo.MSP_EpmTask_UserView ON
 	ProjectWebApp.dbo.MSP_EpmProject_UserView.ProjectUID = ProjectWebApp.dbo.MSP_EpmTask_UserView.ProjectUID LEFT OUTER JOIN ProjectWebApp.dbo.MSP_EpmAssignmentByDay_UserView
@@ -80,34 +75,15 @@ From
 	WHERE
 	(ProjectWebApp.dbo.MSP_EpmAssignmentByDay_UserView.AssignmentActualWork > 0) AND
 	(ProjectWebApp.dbo.MSP_EpmResource_UserView.СДРес like '%КБЭ%' and ProjectWebApp.dbo.MSP_EpmTask_UserView.TaskName like '%ведомость%')
-group by MSP_EpmProject_UserView.[№ заказа]) as KBEFinishTable
-
-select
-*
-from
-(
-SELECT [PortalKATEK].[dbo].[View_RKD_ListMailVersion].[PlanZakaz] as [№заказа]
-      ,min([PortalKATEK].[dbo].[View_RKD_ListMailVersion].[dateEvent]) as [Дата готовности первой версии РКД]
-      ,[PortalKATEK].[dbo].[View_RKD_ListMailVersion].[text]
-FROM [PortalKATEK].[dbo].[View_RKD_ListMailVersion]
-where
-[PortalKATEK].[dbo].[View_RKD_ListMailVersion].[text] like 'Отправлена черновая версия РКД в ТП'
-group by [PortalKATEK].[dbo].[View_RKD_ListMailVersion].[text]
-,[PortalKATEK].[dbo].[View_RKD_ListMailVersion].[PlanZakaz]
-order by
-[PortalKATEK].[dbo].[View_RKD_ListMailVersion].[PlanZakaz]
-) as StartWorkToRKDTable
-
-select
-*
-from
+	group by MSP_EpmProject_UserView.[№ заказа]) as KBEFinishTable on ManufacturingTable.[№заказа] = KBEFinishTable.[№заказа] left join
 (SELECT [PortalKATEK].[dbo].[View_RKD_ListMailVersion].[PlanZakaz] as [№заказа]
-      ,min([PortalKATEK].[dbo].[View_RKD_ListMailVersion].[dateEvent]) as [Дата согласования РКД]
-      ,[PortalKATEK].[dbo].[View_RKD_ListMailVersion].[text]
-FROM [PortalKATEK].[dbo].[View_RKD_ListMailVersion]
-where
-[PortalKATEK].[dbo].[View_RKD_ListMailVersion].[text] like 'Получено согласование РКД'
-group by [PortalKATEK].[dbo].[View_RKD_ListMailVersion].[text]
-,[PortalKATEK].[dbo].[View_RKD_ListMailVersion].[PlanZakaz]
-order by
-[PortalKATEK].[dbo].[View_RKD_ListMailVersion].[PlanZakaz]) as ComplitedRKDTable
+    ,min([PortalKATEK].[dbo].[View_RKD_ListMailVersion].[dateEvent]) as [Дата готовности первой версии РКД]
+	FROM [PortalKATEK].[dbo].[View_RKD_ListMailVersion]
+    where
+	[PortalKATEK].[dbo].[View_RKD_ListMailVersion].[text] like 'Отправлена черновая версия РКД в ТП'
+	group by [PortalKATEK].[dbo].[View_RKD_ListMailVersion].[PlanZakaz]) as StartWorkToRKDTable on ManufacturingTable.[№заказа] = StartWorkToRKDTable.[№заказа] left join
+(SELECT [PortalKATEK].[dbo].[View_RKD_ListMailVersion].[PlanZakaz] as [№заказа]
+    ,min([PortalKATEK].[dbo].[View_RKD_ListMailVersion].[dateEvent]) as [Дата согласования РКД]
+	FROM [PortalKATEK].[dbo].[View_RKD_ListMailVersion]
+	where [PortalKATEK].[dbo].[View_RKD_ListMailVersion].[text] like 'Получено согласование РКД'
+	group by [PortalKATEK].[dbo].[View_RKD_ListMailVersion].[PlanZakaz]) as ComplitedRKDTable on ManufacturingTable.[№заказа] = ComplitedRKDTable.[№заказа]
