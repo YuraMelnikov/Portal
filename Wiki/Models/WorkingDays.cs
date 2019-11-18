b25e8c828783172bf84f3a18d85d5a2c1abea5a9
YuraMelnikov <yury.melnikau@gmail.com>
2019-11-18 09:01:26 +0300

    -

--- Wiki/Models/WorkingDays.cs
+++ Wiki/Models/WorkingDays.cs
@@ -7,11 +7,18 @@ namespace Wiki.Models
     {
         public int GetWorkingDays(DateTime from, DateTime to)
         {
-            var dayDifference = (int)to.Subtract(from).TotalDays;
-            return Enumerable
-                .Range(1, dayDifference)
-                .Select(x => from.AddDays(x))
-                .Count(x => x.DayOfWeek != DayOfWeek.Saturday && x.DayOfWeek != DayOfWeek.Sunday);
+            try
+            {
+                var dayDifference = (int)to.Subtract(from).TotalDays;
+                return Enumerable
+                    .Range(1, dayDifference)
+                    .Select(x => from.AddDays(x))
+                    .Count(x => x.DayOfWeek != DayOfWeek.Saturday && x.DayOfWeek != DayOfWeek.Sunday);
+            }
+            catch
+            {
+                return 0;
+            }
         }
     }
 }
