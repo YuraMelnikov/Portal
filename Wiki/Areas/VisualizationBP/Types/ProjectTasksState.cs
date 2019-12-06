b78a38f7c7dc2cd47abb70cf90667f325751f493
YuraMelnikov <yury.melnikau@gmail.com>
2019-12-06 08:24:01 +0300

    complited class ProjectTasksState

--- Wiki/Areas/VisualizationBP/Types/ProjectTasksState.cs
+++ Wiki/Areas/VisualizationBP/Types/ProjectTasksState.cs
@@ -3,5 +3,17 @@
     public class ProjectTasksState
     {
         BlockProjectTasksState[] blockProjectTasksStates;
+
+        public ProjectTasksState()
+        {
+            int countBlocks = 5;
+            BlockProjectTasksStates = new BlockProjectTasksState[countBlocks];
+            for (int i = 0; i < countBlocks; i++)
+            {
+                BlockProjectTasksStates[i] = new BlockProjectTasksState();
+            }
+        }
+
+        public BlockProjectTasksState[] BlockProjectTasksStates { get => blockProjectTasksStates; set => blockProjectTasksStates = value; }
     }
 }
