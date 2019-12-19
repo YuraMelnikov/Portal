5a835789b0afc45a0d8569fa9b97c009d9854f41
YuraMelnikov <yury.melnikau@gmail.com>
2019-12-19 14:16:34 +0300

    PZ: send mail before edit nameTU

--- Wiki/Areas/PZ/Controllers/OrderController.cs
+++ Wiki/Areas/PZ/Controllers/OrderController.cs
@@ -746,6 +746,8 @@ namespace Wiki.Areas.PZ.Controllers
             PZ_PlanZakaz editPZ = db.PZ_PlanZakaz.First(d => d.PlanZakaz == pZ_PlanZakaz.PlanZakaz);
             if (editPZ.nameTU != pZ_PlanZakaz.nameTU)
             {
+                EmailRename emailRename = new EmailRename(editPZ.PlanZakaz.ToString(), editPZ.nameTU, pZ_PlanZakaz.nameTU, login, true);
+                emailRename.SendEmail();
                 editPZ.nameTU = pZ_PlanZakaz.nameTU;
                 editPZ.ProductType = pZ_PlanZakaz.ProductType;
             }
