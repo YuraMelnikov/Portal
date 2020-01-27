2252334fc67cc0f59a065c2e57d5b96d93e7696d
YuraMelnikov <yury.melnikau@gmail.com>
2020-01-27 09:06:48 +0300

    -

--- Wiki/Areas/Cells/Controllers/SCellsController.cs
+++ Wiki/Areas/Cells/Controllers/SCellsController.cs
@@ -2,6 +2,7 @@
 using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
+using System;
 
 namespace Wiki.Areas.Cells.Controllers
 {
@@ -13,8 +14,10 @@ namespace Wiki.Areas.Cells.Controllers
         {
             SCellsEntities db = new SCellsEntities();
             string login = HttpContext.User.Identity.Name;
-            ViewBag.list = new SelectList(db.Section.OrderBy(d => d.name), "idS", "name");
-            ViewBag.sections = new SelectList(db.Section.OrderBy(d => d.name), "idS", "name");
+            ViewBag.list = new SelectList(db.Tier.OrderBy(d => d.name), "id", "name");
+            ViewBag.sections = new SelectList(db.Tier.OrderBy(d => d.name), "id", "name");
+            //ViewBag.list = new SelectList(db.Section.OrderBy(d => d.name), "idS", "name");
+            //ViewBag.sections = new SelectList(db.Section.OrderBy(d => d.name), "idS", "name");
             try
             {
                 logger.Debug("SCells ActionResult: " + login);
@@ -28,32 +31,44 @@ namespace Wiki.Areas.Cells.Controllers
 
         public JsonResult List(string id)
         {
+            int idT = Convert.ToInt32(id);
             string login = HttpContext.User.Identity.Name;
             using (SCellsEntities db = new SCellsEntities())
             {
                 db.Configuration.ProxyCreationEnabled = false;
                 db.Configuration.LazyLoadingEnabled = false;
                 int idS = db.Section.First(d => d.name == id).idS;
-                var query = db.SectionMap
+                var query = db.TierMap
                     .AsNoTracking()
-                    .Include(d => d.Section)
-                    .Include(d => d.Section1)
-                    .Where(d => d.sectionIdStart == idS)
+                    .Include(d => d.Tier)
+                    .Include(d => d.Tier1)
+                    .Where(d => d.id == idT)
                     .ToList();
                 var data = query.Select(dataList => new
                 {
                     editLink = "<td><a href=" + '\u0022' + "#" + '\u0022' + " onclick=" + '\u0022' + "return getPoint('" + dataList.id + "')" + '\u0022' + "><span class=" + '\u0022' + "glyphicon glyphicon-pencil" + '\u0022' + "></span></a></td>",
-
-                    dataList.sectionIdStart,
-                    name1 = "<td><a href=" + '\u0022' + "#" + '\u0022' + " onclick=" + '\u0022' + "return getPoint('" + dataList.id + "')" + '\u0022' + ">" + dataList.Section.name + "</a></td>",
-
-
-
-                    dataList.sectionIdFinish,
-                    name2 = "<td><a href=" + '\u0022' + "#" + '\u0022' + " onclick=" + '\u0022' + "return getPoint('" + dataList.id + "')" + '\u0022' + ">" + dataList.Section1.name + "</a></td>",
+                    sectionIdStart = dataList.tierIdStart,
+                    name1 = "<td><a href=" + '\u0022' + "#" + '\u0022' + " onclick=" + '\u0022' + "return getPoint('" + dataList.id + "')" + '\u0022' + ">" + dataList.Tier.name + "</a></td>",
+                    sectionIdFinish = dataList.tierIdFinish,
+                    name2 = "<td><a href=" + '\u0022' + "#" + '\u0022' + " onclick=" + '\u0022' + "return getPoint('" + dataList.id + "')" + '\u0022' + ">" + dataList.Tier1.name + "</a></td>",
                     dataList.distance
                 });
-
+                //int idS = db.Section.First(d => d.name == id).idS;
+                //var query = db.SectionMap
+                //    .AsNoTracking()
+                //    .Include(d => d.Section)
+                //    .Include(d => d.Section1)
+                //    .Where(d => d.sectionIdStart == idS)
+                //    .ToList();
+                //var data = query.Select(dataList => new
+                //{
+                //    editLink = "<td><a href=" + '\u0022' + "#" + '\u0022' + " onclick=" + '\u0022' + "return getPoint('" + dataList.id + "')" + '\u0022' + "><span class=" + '\u0022' + "glyphicon glyphicon-pencil" + '\u0022' + "></span></a></td>",
+                //    dataList.sectionIdStart,
+                //    name1 = "<td><a href=" + '\u0022' + "#" + '\u0022' + " onclick=" + '\u0022' + "return getPoint('" + dataList.id + "')" + '\u0022' + ">" + dataList.Section.name + "</a></td>",
+                //    dataList.sectionIdFinish,
+                //    name2 = "<td><a href=" + '\u0022' + "#" + '\u0022' + " onclick=" + '\u0022' + "return getPoint('" + dataList.id + "')" + '\u0022' + ">" + dataList.Section1.name + "</a></td>",
+                //    dataList.distance
+                //});
                 return Json(new { data, MaxJsonLength = int.MaxValue });
             }
         }
@@ -64,20 +79,34 @@ namespace Wiki.Areas.Cells.Controllers
             {
                 db.Configuration.ProxyCreationEnabled = false;
                 db.Configuration.LazyLoadingEnabled = false;
-                var query = db.SectionMap
-                    .Include(d => d.Section)
-                    .Include(d => d.Section1)
+                var query = db.TierMap
+                    .Include(d => d.Tier)
+                    .Include(d => d.Tier1)
                     .Where(d => d.id == id)
                     .ToList();
                 var data = query.Select(dataList => new
                 {
                     dataList.id,
-                    dataList.sectionIdStart,
-                    name1 = dataList.Section.name,
-                    dataList.sectionIdFinish,
-                    name2 = dataList.Section1.name,
+                    sectionIdStart = dataList.tierIdStart,
+                    name1 = dataList.Tier.name,
+                    sectionIdFinish = dataList.tierIdFinish,
+                    name2 = dataList.Tier1.name,
                     dataList.distance
                 });
+                //var query = db.SectionMap
+                //    .Include(d => d.Section)
+                //    .Include(d => d.Section1)
+                //    .Where(d => d.id == id)
+                //    .ToList();
+                //var data = query.Select(dataList => new
+                //{
+                //    dataList.id,
+                //    dataList.sectionIdStart,
+                //    name1 = dataList.Section.name,
+                //    dataList.sectionIdFinish,
+                //    name2 = dataList.Section1.name,
+                //    dataList.distance
+                //});
                 return Json(data.First(), JsonRequestBehavior.AllowGet);
             }
         }
@@ -102,16 +131,35 @@ namespace Wiki.Areas.Cells.Controllers
                         int predcessor = sectionsChosen[i];
                         int finalId = sectionsChosen[j];
                         distanceFinal += distanceSelections;
-                        SectionMap sCells = db.SectionMap.First(d => d.sectionIdStart == predcessor && d.sectionIdFinish == finalId);
+                        TierMap sCells = db.TierMap.First(d => d.tierIdStart == predcessor && d.tierIdFinish == finalId);
                         sCells.distance = distanceFinal;
                         db.Entry(sCells).State = EntityState.Modified;
                         db.SaveChanges();
-                        SectionMap sCells1 = db.SectionMap.First(d => d.sectionIdFinish == sCells.sectionIdStart && d.sectionIdStart == sCells.sectionIdFinish);
-                        sCells1.distance = distanceFinal;
-                        db.Entry(sCells1).State = EntityState.Modified;
-                        db.SaveChanges();
+
+                        //SectionMap sCells1 = db.SectionMap.First(d => d.sectionIdFinish == sCells.sectionIdStart && d.sectionIdStart == sCells.sectionIdFinish);
+                        //sCells1.distance = distanceFinal;
+                        //db.Entry(sCells1).State = EntityState.Modified;
+                        //db.SaveChanges();
                     }
                 }
+                //for (int i = 0; i < sectionsChosen.Length; i++)
+                //{
+                //    double distanceFinal = 0;
+                //    for (int j = i + 1; j < sectionsChosen.Length; j++)
+                //    {
+                //        int predcessor = sectionsChosen[i];
+                //        int finalId = sectionsChosen[j];
+                //        distanceFinal += distanceSelections;
+                //        SectionMap sCells = db.SectionMap.First(d => d.sectionIdStart == predcessor && d.sectionIdFinish == finalId);
+                //        sCells.distance = distanceFinal;
+                //        db.Entry(sCells).State = EntityState.Modified;
+                //        db.SaveChanges();
+                //        SectionMap sCells1 = db.SectionMap.First(d => d.sectionIdFinish == sCells.sectionIdStart && d.sectionIdStart == sCells.sectionIdFinish);
+                //        sCells1.distance = distanceFinal;
+                //        db.Entry(sCells1).State = EntityState.Modified;
+                //        db.SaveChanges();
+                //    }
+                //}
             }
             logger.Debug("SCells UpdatePointsCells");
             return Json(1, JsonRequestBehavior.AllowGet);
