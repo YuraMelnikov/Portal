9d2ecb31705332463fb97c8606cfe4061c411778
YuraMelnikov <yury.melnikau@gmail.com>
2020-07-02 11:49:21 +0300

    -

---
+++ Wiki/Areas/CMOS/CMOSAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Wiki.Areas.CMOS
+{
+    public class CMOSAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "CMOS";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "CMOS_default",
+                "CMOS/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
